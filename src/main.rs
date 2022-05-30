7f36b4a66b1f34c799db229188644f5828a16171
ynhhoJ <lJohhnyl@outlook.com>
2022-05-30 21:30:41 +0300

    🚧 Add bot what send "Hello World" as reply of message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod types;
+mod variables;
+
+use teloxide::{adaptors::throttle::Limits, prelude::*, repl};
+use types::bot::BotWithAutoSendAndThrottle;
+use variables::TELOXIDE_TOKEN;
+
+#[tokio::main]
+async fn main() {
+    let bot = Bot::new(TELOXIDE_TOKEN.as_str())
+        .throttle(Limits::default())
+        .auto_send();
+
+    repl(
+        bot,
+        |message: Message, bot: BotWithAutoSendAndThrottle| async move {
+            let chat_id = message.chat.id;
+
+            bot.send_message(chat_id, "Hello world!").await?;
+
+            respond(())
+        },
+    )
+    .await;
 }
