98176745ea2027ea284ba25fb93d385b6156b39e
ynhhoJ <lJohhnyl@outlook.com>
2022-06-08 20:31:36 +0300

    🚧 Implement inline query handler

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
+mod handlers;
 mod parser;
 mod types;
 mod utils;
 mod variables;
 
-use parser::main::main as parser_main;
-use teloxide::{adaptors::throttle::Limits, prelude::*, repl};
-use types::bot::BotWithAutoSendAndThrottle;
+use handlers::inline_query::inline_query;
+use teloxide::{adaptors::throttle::Limits, prelude::*};
 use variables::TELOXIDE_TOKEN;
 
 #[tokio::main]
 async fn main() {
-    let game_name = "Bioshock";
-    let parser_response = parser_main(game_name.to_string()).await;
-    println!("parser response: {:#?}", parser_response);
-
     let bot = Bot::new(TELOXIDE_TOKEN.as_str())
         .throttle(Limits::default())
         .auto_send();
 
-    repl(
-        bot,
-        |message: Message, bot: BotWithAutoSendAndThrottle| async move {
-            let chat_id = message.chat.id;
+    let handler = inline_query().await;
 
-            bot.send_message(chat_id, "Hello world!").await?;
-
-            respond(())
-        },
-    )
-    .await;
+    Dispatcher::builder(bot, handler)
+        .build()
+        .setup_ctrlc_handler()
+        .dispatch()
+        .await;
 }
